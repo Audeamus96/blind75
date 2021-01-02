e4be582d7bccc6a0feb5e441c11dcf1e678471ca
Josh Murphy <joshua0833@hotmail.com>
2021-01-02 00:08:40 -0800

    slight improvement to q2

--- array/q2.cpp
+++ array/q2.cpp
@@ -7,19 +7,17 @@ int maxProfit(std::vector<int>& prices){
 	if (prices.size() == 0) return 0;
 	
 	int ans = 0;
-	int curMin, curMax;
-	curMin = curMax = prices[0];
+	int curMin = prices[0];
 
 	for(int i=0; i < prices.size(); i++){
 		curMin = std::min(curMin, prices[i]);
-		curMax = std::max(curMin, prices[i]);
-
-		ans = std::max((curMax-curMin), ans);
+		ans = std::max((prices[i]-curMin), ans);
 	}
 
 	return ans;
 }
 
+//Tests
 bool testCase1(){
 	std::vector<int> test{7,1,5,3,6,4};
 	int excpected = 5;
